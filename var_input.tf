bc42f8ed8e798277909ab50d613a04dfbf028d42
Mike Peachey <mike.peachey@bjss.com>
2016-08-10 22:58:10 +0100

    Additional inputs and cleanup of required inputs based on new ODP diagram

--- var_input.tf
+++ var_input.tf
@@ -1,81 +1,98 @@
 ### Module inputs ###
 
-# Service tags
-variable "service_tags" {
-  type = "map"
-  default = {
-  }
+variable "asg_health_check_grace_period" {
+  type        = "string"
+  default     = "300"
+  description = "Time (in seconds) after instance comes into service before checking health"
 }
 
-variable "ssh_key_name" {
-  type    = "string"
-  default = "AWS_SSH_KEY_NAME"
+variable "asg_health_check_type" {
+  type        = "string"
+  default     = "EC2"
+  description = "'EC2' or 'ELB'. Controls how health checking is done"
 }
 
-# VPC ID for microservice
+variable "asg_size_max" {
+  type        = "string"
+  default     = "1"
+  description = "The maximum size of the autoscaling group"
+}
+
+variable "asg_size_min" {
+  type        = "string"
+  default     = "1"
+  description = "The minimum size of the autoscaling group"
+}
+
+variable "asg_termination_policies" {
+  type        = "list"
+  default     = ["OldestLaunchConfiguration", "ClosestToNextInstanceHour"]
+  description = "A list of policies to decide how the instances in the auto scale group should be terminated"
+}
+
+variable "availability_zones" {
+  type        = "list"
+  default     = []
+  description = "List of Availability Zones for Subnets and Autoscaling Groups"
+}
+
+variable "aws_region" {
+  type        = "string"
+  description = "The AWS region"
+}
+
+variable "iam_policies" {
+  type        = "list"
+  default     = []
+  description = "A list of IAM policies to associate with the EC2 role used to create EC2 instance profiles"
+}
+
+variable "lc_ami_id" {
+  type        = "string"
+  default     = ""
+  description = "The AMI ID to use in the ASG Launch Configuration"
+}
+
+variable "lc_instance_type" {
+  type        = "string"
+  description = "The microservice EC2 instance type"
+}
+
+variable "load_balancers" {
+  type        = "list"
+  default     = []
+  description = "A list of load balancers to associate to the ASG"
+}
+
+variable "name" {
+  type        = "string"
+  description = "Microservice name"
+}
+
+variable "subnets_cidr" {
+  type        = "list"
+  default     = []
+  description = "List of CIDR blocks for microservice subnets"
+}
+
+variable "subnets_map_public_ip_on_launch" {
+  type        = "string"
+  default     = "0"
+  description = "Specify true to indicate that instances should be assigned a public IP address"
+}
+
+variable "subnets_route_table_id" {
+  type        = "string"
+  description = "Route table ID to associate with the subnets"
+}
+
+variable "tags" {
+  type        = "map"
+  default     = {}
+  description = "Tags to apply to all components within the microservice"
+}
+
 variable "vpc_id" {
-  type    = "string"
-}
-
-# CIDR list for private networks
-variable "subnets_cidr_priv" {
-  type    = "list"
-  default = []
-}
-
-# CIDR list for public networks
-variable "subnets_cidr_pub" {
-  type    = "list"
-  default = []
-}
-
-# List of az for microservice
-variable "subnets_availability_zones" {
-  type    = "list"
-  default = ["eu-west-1a","eu-west-1b","eu-west-1c"]
-}
-
-# Enable asg for the service
-variable "enable_asg" {
-  type    = "string"
-  default = "1"
-}
-
-# Number of instances in autoscaling group
-variable "asg_instance_count" {
-  type    = "string"
-  default = "1"
-}
-
-
-# AMI ID for used for microservice
-variable "ami_id" {
-  type    = "string"
-  default = ""
-}
-
-# Instance type for service ( t2.micro :)
-variable "instance_type" {
-  type    = "string"
-  default = ""
-}
-
-# Number of ec2 stand alone instances.
-# If we set this variable our service will be deployed as stand alone ec2 instance
-variable "stand_alone_instance_count" {
-  type    = "string"
-  default = "0"
-}
-
-
-# Assign public ip to ASG
-variable "public_ip_toggle" {
-  type    = "string"
-  default = "0"
-}
-
-# Route 53 zone id for Environment
-variable "route53_zone_id" {
-  type    = "string"
-  default = ""
+  type        = "string"
+  description = "Parent VPC ID"
 }
