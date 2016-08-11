f34847dfc12cdddc822d6533c9c2b93c9ba5186e
Mike Peachey <mike.peachey@bjss.com>
2016-08-11 22:13:40 +0100

    basic non-dependent SGs

--- sg.tf
+++ sg.tf
@@ -11,3 +11,19 @@ resource "aws_security_group" "security_group" {
     Role        = "${var.name}"
   }
 }
+
+resource "aws_security_group_rule" "allow_egress_all" {
+  type        = "egress"
+  protocol    = "-1"
+  from_port   = 0
+  to_port     = 0
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "allow_ingress_self" {
+  type      = "ingress"
+  protocol  = "-1"
+  from_port = 0
+  to_port   = 0
+  self      = true
+}
