0ee0d965de20532f1b793103653365a7816ecb90
Mike Peachey <mike.peachey@bjss.com>
2016-08-16 11:57:30 +0100

    Repo move to cartest

--- subnets.tf
+++ subnets.tf
@@ -1,5 +1,5 @@
 module "subnets" {
-  source                  = "github.com/Zordrak/terraform-subnet-tuple.git"
+  source                  = "github.com/cartest/terraform-subnet-tuple.git"
   availability_zones      = "${var.availability_zones}"
   aws_region              = "${var.aws_region}"
   cidrs                   = "${var.subnets_cidr}"
