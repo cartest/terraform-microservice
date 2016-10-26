5540ec613c58c90ef92a59160a3daeb044457396
Jeremy Bowles <jeremybo@wiggum.local>
2016-10-26 09:50:59 +0100

    replacing color var

--- sg.tf
+++ sg.tf
@@ -1,10 +1,10 @@
 resource "aws_security_group" "security_group" {
-  name        = "${var.tags["Environment"]}-${var.tags["Application"]}-${var.tags["Tier"]}-${var.name}-${var.app-stack-color}-SG"
-  description = "${var.tags["Environment"]}-${var.tags["Application"]}-${var.tags["Tier"]}-${var.name}-${var.app-stack-color}-SG"
+  name        = "${var.tags["Environment"]}-${var.tags["Application"]}-${var.tags["Tier"]}-${var.name}-${var.app-stack-identifier}-SG"
+  description = "${var.tags["Environment"]}-${var.tags["Application"]}-${var.tags["Tier"]}-${var.name}-${var.app-stack-identifier}-SG"
   vpc_id      = "${var.vpc_id}"
 
   tags {
-    Name        = "${var.tags["Environment"]}-${var.tags["Application"]}-${var.tags["Tier"]}-${var.name}-${var.app-stack-color}-SG"
+    Name        = "${var.tags["Environment"]}-${var.tags["Application"]}-${var.tags["Tier"]}-${var.name}-${var.app-stack-identifier}-SG"
     Environment = "${var.tags["Environment"]}"
     Application = "${var.tags["Application"]}"
     Tier        = "${var.tags["Tier"]}"
