76064b71a6fefadada8abc15a805327a956551be
Jeremy Bowles <jeremybo@wiggum.local>
2016-11-03 09:47:09 +0000

    more lifecycle testing

--- asg.tf
+++ asg.tf
@@ -6,12 +6,12 @@ resource "aws_launch_configuration" "launch_configuration" {
   security_groups      = ["${concat(split(",", aws_security_group.security_group.id), var.additional_security_group_ids)}"]
   iam_instance_profile = "${aws_iam_instance_profile.iam_instance_profile.id}"
 
-  #user_data            = "${var.user_data_script}"
+  user_data            = "${var.user_data_script}"
   #key_name             = "${var.lc_key_name}"
 
-  #lifecycle {
-  #   create_before_destroy = true
-  #}
+  lifecycle {
+     create_before_destroy = false
+  }
 }
 
 # Create ASG and assing launch configration to it
