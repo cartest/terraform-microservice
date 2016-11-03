a6e54759764805a5d4fcbac4e4b22d359104bab5
Jeremy Bowles <jeremybo@wiggum.local>
2016-11-03 09:10:54 +0000

    testing lifecycle destroy cycle errors

--- asg.tf
+++ asg.tf
@@ -9,9 +9,9 @@ resource "aws_launch_configuration" "launch_configuration" {
   user_data            = "${var.user_data_script}"
   #key_name             = "${var.lc_key_name}"
 
-  lifecycle {
-    create_before_destroy = true
-  }
+  #lifecycle {
+  #   create_before_destroy = true
+  #}
 }
 
 # Create ASG and assing launch configration to it
